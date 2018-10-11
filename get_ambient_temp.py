ec061d33cbf2d26ab3864f1a6dfff359c16fc8d5
Nora Lutz <nora.e.lutz@nasa.gov>
2018-10-11 10:01:00 -0400

    compacting

--- get_ambient_temp.py
+++ get_ambient_temp.py
@@ -7,28 +7,20 @@ import sys
 
 def get_temp(host,ilo_user,ilo_pass):
 	ilo = hpilo.Ilo(host,ilo_user,ilo_pass)
-#	health_str = json.dumps(ilo.get_embedded_health())
-	health_dict = json.loads(json.dumps(ilo.get_embedded_health()))
+	health = ilo.get_embedded_health()
 	#get the current Inlet Ambient temp from the sensor, in Celcius
-	temp_c = health_dict['temperature']['01-Inlet Ambient']['currentreading'][0]
+	temp_c = health['temperature']['01-Inlet Ambient']['currentreading'][0]
 	#convert to fahrenheit
 	temp_f = int(temp_c) * 9/5 + 32
         return temp_f
 
 if len(sys.argv) < 5:
-	print("usage: get_ambient_temp.py admin_user ilo_password inventory_filename warn_temp")
-	sys.exit(1)
-
+       	print("usage: get_ambient_temp.py admin_user ilo_password inventory_filename warn_temp")
+        sys.exit(1)
 else:
-	ilo_user = str(sys.argv[1])
-	ilo_pass = str(sys.argv[2])
-	inventory_file = str(sys.argv[3])
-	warn_temp = int(sys.argv[4])
-
-	inventory = open(inventory_file, 'r')
+	inventory = open(str(sys.argv[3]), 'r')
 	for line in inventory.readlines():
-        	host = line.split()
-		temp = get_temp(host[0],ilo_user,ilo_pass)
-		if temp >= warn_temp:
-			print("WARNING. The temperature is {} degrees, exceeded {} degrees.".format(temp,warn_temp))
-  			sys.exit(0)
+               	host = line.split()
+               	temp = get_temp(host[0],str(sys.argv[1]),str(sys.argv[2]))
+               	if temp >= int(sys.argv[4]):
+                       	print("WARNING. The temperature is {0} degrees, exceeded {1} degrees.".format(temp,int(sys.argv[4])))
